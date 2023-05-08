4115b8abc91d55f406546dc05344cfb68b95cca8
Graham MacFarquhar <gmacfarquhar57@gmail.com>
2023-05-08 16:55:45 -0700

    code refactor after review

--- numbers/numbers.js
+++ numbers/numbers.js
@@ -2,43 +2,41 @@
 
 const $contentArea = $("#content-area");
 const NUMBERS_API_URL = "http://numbersapi.com";
-
-async function oneFact(number){
+//TODO: Clean up names, map function,
+async function getOneFact(number){
   const response = await axios(`${NUMBERS_API_URL}/${number}`, { params: { q: 'json' } });
   return response.data;
 }
 
-async function onePromise(number){
-  const response = axios(`${NUMBERS_API_URL}/${number}`, { params: { q: 'json' } });
-  return response;
-}
-
-async function multipleFacts(numbers){
+async function getMultipleFacts(numbers){
   const response = await axios(`${NUMBERS_API_URL}/${numbers}`, { params: { q: 'json' } });
-  let facts = "";
+  let facts = ""; //TODO: Map functions
   for(let key in response.data){
     facts += response.data[key] + " "
   }
   return facts;
 }
 
-async function fourFacts(number){
-  const resultsPromise = await Promise.all(
-    [onePromise(number), onePromise(number), onePromise(number), onePromise(number)]);
+async function getFourFacts(number){
+  const resultsPromise = await Promise.allSettled(
+    [getOneFact(number), getOneFact(number), getOneFact(number), getOneFact(number)]);
 
   let facts = "";
   for(let fact of resultsPromise){
-    facts += fact.data + " "
+    facts += fact.value + " "
   }
   return facts;
 }
 
 async function appendToContent(promise){
   let str = await promise
+  console.log("appended", str)
   $contentArea.append($('<p>').html(str))
 }
 
-appendToContent(oneFact(7))
-appendToContent(multipleFacts("7,5,3,10,11"))
-appendToContent(fourFacts(7))
+appendToContent(getOneFact(7))
+console.log("passed first append")
+appendToContent(getMultipleFacts("7,5,3,10,11"))
+console.log("passed second append")
+appendToContent(getFourFacts(7))
 
